d765cd71505f76a68ae4d7f1eb737748884bb426
Joseph Gunn <joe@josephgunn.com>
2013-04-05 17:12:50 -0400

    Make usefull for benchmarking

--- Utilities/eyes.py
+++ Utilities/eyes.py
@@ -3,13 +3,21 @@ import cv
 import cv2
 import numpy as np
 
+imageCount = 0
+faceCount = 0
+eyeCount = 0
+#faceFile = "/home/gunnjo/NetSrc/opencv/data/haarcascades/haarcascade_frontalface_alt.xml"
+faceFile = "/home/gunnjo/NetSrc/opencv/data/haarcascades/haarcascade_frontalface_default.xml"
+eyeFile = "/home/gunnjo/NetSrc/opencv/data/haarcascades/haarcascade_eye.xml"
+eyeFile = "/home/gunnjo/NetSrc/opencv/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml"
+
 def Load():
-	faceCascade = cv2.CascadeClassifier("/usr/share/haarcascade.xml")
-	eyeCascade = cv2.CascadeClassifier("/usr/share/haarcascade_eye.xml")
+	faceCascade = cv2.CascadeClassifier(faceFile)
+	eyeCascade = cv2.CascadeClassifier(eyeFile)
 	return (faceCascade, eyeCascade)
 
-def DetectRedEyes(image, faceCascade, eyeCascade):
-
+def DetectEyes(image, faceCascade, eyeCascade):
+	global faceCount, eyeCount
 	min_size = (30,30)
 	eye_size = (20,20)
 	haar_scale = 1.1
@@ -23,7 +31,9 @@ def DetectRedEyes(image, faceCascade, eyeCascade):
 	faces = faceCascade.detectMultiScale(gray, haar_scale,
 		min_neighbors, haar_flags, min_size)
 	# If faces are found
+	faceFound = 0
 	for (x, y, w, h) in faces:
+		faceFound+=1
 		pt1 = (x , y )
 		pt2 = ((x + w), (y + h))
 		cv2.rectangle(image, pt1, pt2, cv.RGB(255, 0, 0), 3, 8, 0)
@@ -42,7 +52,9 @@ def DetectRedEyes(image, faceCascade, eyeCascade):
 		# If eyes were found
 		print "Eyes ",len(eyes)
 		# For each eye found
+		eyeFound = 0
 		for (ex, ey,ew,eh) in eyes:
+			eyeFound+=1
 			# Draw a rectangle around the eye
 			cv2.rectangle(image,
 				(ex+pt1[0],
@@ -51,22 +63,40 @@ def DetectRedEyes(image, faceCascade, eyeCascade):
 				ey++pt1[1]+eh),
 				cv.RGB(255, 0, 0), 1, 8, 0)
 
+		if ( eyeFound > 0):
+			eyeCount+=1
+	if ( faceFound > 0):
+		faceCount+=1
 	return image
+
+def GoAway():
+	print "Images", imageCount, "faces", faceCount, "eyes", eyeCount, "faceCascade", faceFile, "eyeCascade", eyeFile
+	exit()
 
 if __name__ == "__main__":
 	if len(sys.argv)==1:
 		capture = cv2.VideoCapture( 0 )
-	elif len(sys.argv)==2 and sys.argv[1].isdigit():
+	elif len(sys.argv)>1 and sys.argv[1].isdigit():
 		capture = cv2.VideoCapture( int(sys.argv[1]) )
-	elif len(sys.argv)==2:
+	elif len(sys.argv)>1:
 		capture = cv2.VideoCapture( sys.argv[1] )
+
+	if (len(sys.argv) > 2):
+		faceFile = sys.argv[2]
+
+	if len(sys.argv)>3:
+		eyeFile = sys.argv[3]
 
 	cv2.namedWindow("w1")
 	faceCascade, eyeCascade = Load()
 	endit = 1
 	while endit:
 		(ret, image) = capture.read()
-		image = DetectRedEyes(image, faceCascade, eyeCascade)
-		cv2.imshow("w1", image)
-		if (cv2.waitKey(10000) == 27):
-			endit = 0
+		if ( ret):
+			imageCount+=1
+			image = DetectEyes(image, faceCascade, eyeCascade)
+			cv2.imshow("w1", image)
+#			if (cv2.waitKey(10000) == 27):
+#				endit = 0
+		else : endit = 0
+	GoAway()
