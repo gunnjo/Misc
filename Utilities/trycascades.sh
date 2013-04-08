5f1037089c6e62c61719b8f8d3967a414efa17c2
Joseph Gunn <joe@josephgunn.com>
2013-04-08 09:59:05 -0400

    Allow use of other than the GENKI video

--- Utilities/trycascades.sh
+++ Utilities/trycascades.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+F="~/vision_video/GENKI-R2009a.avi"
+[ "x$1" != "x" ] && F=$1
 for x in /home/gunnjo/NetSrc/opencv/data/haarcascades/haarcascade_frontalface_alt_tree.xml /home/gunnjo/NetSrc/opencv/data/haarcascades/haarcascade_frontalface_alt2.xml /home/gunnjo/NetSrc/opencv/data/haarcascades/haarcascade_frontalface_default.xml /home/gunnjo/NetSrc/opencv/data/haarcascades/haarcascade_frontalface_alt.xml
 do
 	for y in /home/gunnjo/NetSrc/opencv/data/haarcascades/haarcascade_eye.xml /home/gunnjo/NetSrc/opencv/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml
 	do
-		RUN=`date "+%Y%m%d%H%M"`
-		time python ./eyes.py  ~/vision_video/GENKI-R2009a.avi $x $y > $RUN.out 2>$RUN.err
+		RUN=`date "+%Y%m%d%H%M%S"`
+		time python ./eyes.py  $F $x $y > $RUN.out 2>$RUN.err
 	done
 done
