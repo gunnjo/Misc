6ddeeb815348def86e48f7047cc8d0a434c50b0b
Joseph Gunn <joe@josephgunn.com>
2014-07-01 17:14:26 -0400

    tuning

--- toys/boxes.py
+++ toys/boxes.py
@@ -9,16 +9,20 @@ global mem
 
 def doalgo( image):
     gray = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY )
-    corners = cv2.goodFeaturesToTrack( gray, 8, .1, 10 )
+    corners = cv2.goodFeaturesToTrack( gray, 20, .1, 100 )
     corners = np.int0(corners)
     print "Trying ", len(corners)
 
     for corner in corners:
         x,y = corner.ravel()
         print "x=", x,  " y=", y
-        cv2.circle( image, (x,y), 10, (255, 0, 0), -1)
+        cv2.circle( image, (x,y), 10, (255, 0, 0), -3)
 
     return gray
+
+def scaleshow( win, image):
+    i = cv2.resize( image, (int(image.shape[1]*.25), int(image.shape[0]*.25)), 0, 0, cv2.INTER_NEAREST)
+    cv2.imshow( win, i )
 
 if __name__ == "__main__":
     capture = None 
@@ -31,25 +35,26 @@ if __name__ == "__main__":
     elif len(sys.argv)==2:
         image = cv2.imread( sys.argv[1] ) 
 
-    if not capture.isOpened() and (image is None):
+    if  (image is None) and not capture.isOpened():
         print "Could not initialize capturing..."
         sys.exit(-1)
-        
+       
     cv2.namedWindow( "Camera", 1 )
     cv2.namedWindow( "Algo", 1 )
 
     while True:
-        if ( capture.isOpened() ):
+        if ( capture and capture.isOpened() ):
             ret, image = capture.read( )
         else:
-            ret, image = cv2.imread( sys.argv[1] ) 
+            image = cv2.imread( sys.argv[1] )
+            ret = image is not None 
 
         if( ret ):
             wait = 1
             algoimage=doalgo( image)
-            cv2.imshow( "Camera", image )
+            scaleshow( "Camera", image )
             if( algoimage.any() ):
-                cv2.imshow( "Algo", algoimage )
+                scaleshow( "Algo", algoimage )
                 del algoimage
                 wait = 1000
             if( cv2.waitKey(wait) == 27 ):
