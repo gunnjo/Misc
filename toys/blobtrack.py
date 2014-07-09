d1469e93969195e506288870ddb891301da4d0ec
Joseph Gunn <joe@josephgunn.com>
2014-07-09 18:38:54 -0400

    Attempt to limit blobs by contour

--- toys/blobtrack.py
+++ toys/blobtrack.py
@@ -14,10 +14,32 @@ def scaleshow( win, image):
     i = cv2.resize( image, (width, height), 0, 0, cv2.INTER_NEAREST)
     cv2.imshow( win, i )
 
+def filterContour( contour):
+    minArea = 2.75*1.25 # minimum area in inches
+    ppi = 200.
+    epsilon = 0.1*cv2.arcLength(contour,True)
+    approx = cv2.approxPolyDP(contour,epsilon,True)
+    if ( len(approx) != 4  ): #only square objects
+        return None
+    if ( cv2.isContourConvex(approx) is False ): #That are closed
+        return None
+    area = cv2.contourArea(approx)
+    if area < (minArea*ppi*.9): #only large enough objects
+        return None
+    return(approx)
 
 def doalgo( image, model):
     fgmask = model.apply(image)
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+    gray = image[:,:,1]    
+    ret, edges = cv2.threshold(gray, 170, 255,0)
+    contours, hierarchy = cv2.findContours( edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    interestingContours = []
+    for contour in contours:
+        adjustedContour = filterContour(contour)
+        if adjustedContour is not None:
+            interestingContours.append(adjustedContour)
+    cv2.drawContours(fgmask, interestingContours, -1, 255, 3)
     return fgmask
 
 if __name__ == "__main__":
